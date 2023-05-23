d60549238836d99f78206c42f6b77a6d30ae79a1
Lucas Pugliese de Morais Barros <lucas.barros@banfico.com>
2023-05-23 07:55:35 -0300

    Remove unused imports

--- src/main/java/br/com/pugliese/authorization/entity/User.java
+++ src/main/java/br/com/pugliese/authorization/entity/User.java
@@ -5,7 +5,6 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
-import org.hibernate.validator.constraints.UniqueElements;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
